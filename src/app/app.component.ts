2177bec8f8c0c5d0af8f189e058dfc84fb9256ac
yaddysantos <34655963+yaddysantos@users.noreply.github.com>
2023-02-24 13:58:02 -0500

    detect screen size changes
    Library that help us identify if the screen is mobile, o desktop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,22 @@ import { PrimeNGConfig } from 'primeng/api';
 })
 export class AppComponent implements OnInit {
   
-  constructor(private primengConfig: PrimeNGConfig) {}
+  constructor(
+    private primengConfig: PrimeNGConfig,
+    private breakPointObserver: BreakpointObserver) {
+
+     //detect screen size changes
+     breakPointObserver.observe(["(max-width: 992px)"])
+     .subscribe((result: BreakpointState)=>{
+      console.log(result);
+       if (result.matches) {//Mobile
+         console.log('yes');
+       }else{//Desktop
+         console.log('no');
+       }
+     });
+     
+  }
 
   ngOnInit() {
     this.primengConfig.ripple = true;
